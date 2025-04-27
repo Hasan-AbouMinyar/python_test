559810222e4c10baa65c2b22d81402fa40974a57
Hasan AbouMinyar <among.00.12@gmail.com>
2025-04-28 01:00:59 +0200

    done with that

--- sup.py
+++ sup.py
@@ -1,4 +1,15 @@
-a = "   Welcome to the world of programming with Python!  535  "
-b = 55
-print(str(b) not in a)
+n = [ 0,0,7,7,1,2]
 
+w = ["hasan","ahmed" , "abouminyar"]
+
+m = [ 2000,"hasan",24,"libyan"]
+
+n.append(4)
+n.append(2)
+n.insert(0,"google")
+n.pop()
+n.pop()
+n.pop()
+n.pop()
+
+print(n,m,w)
