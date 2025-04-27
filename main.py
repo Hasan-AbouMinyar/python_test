182505685a5745ff03da9973a01250cf9bf8e959
Hasan AbouMinyar <among.00.12@gmail.com>
2025-04-27 17:37:58 +0200

    lower

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 def main():
     print("Welcome to the world of programming with Python!")
+    a = "Welcome to the world of programming with Python!"
+    print(a.lower())
 
 if __name__ == "__main__":
     main()
