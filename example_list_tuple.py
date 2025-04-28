5c3d131a002769456d36603a13d8e4b6605a0fc4
Hasan AbouMinyar <among.00.12@gmail.com>
2025-04-28 06:17:59 +0200

    done first example

---
+++ example_list_tuple.py
@@ -0,0 +1,17 @@
+list1 = [5,20,15,20 ,25,50,20]
+
+list2 = []
+
+for i in list1 :
+    if i != 20:
+        list2.append(i)
+
+print(list2)
+
+    
+
+
+while 20 in list1 :
+    list1.remove(20)
+
+print(list1)
