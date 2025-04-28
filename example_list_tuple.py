5b44bcc3cac23349e76caa6861408c0046a85c6e
Hasan AbouMinyar <among.00.12@gmail.com>
2025-04-28 06:28:14 +0200

    done for now

--- example_list_tuple.py
+++ example_list_tuple.py
@@ -1,17 +1,38 @@
-list1 = [5,20,15,20 ,25,50,20]
 
-list2 = []
 
-for i in list1 :
-    if i != 20:
-        list2.append(i)
 
-print(list2)
+# list1 = ["a", "b", ["c", ["d", "e", ["f", "g"], "k"], "l"], "m", "n"]
+
+# # list1[2][1][2].extend(["h","j","i"])
+
+# # print(list1)
+
+
+# sup= ["h","j","i"]
+
+# for i in sup: 
+#     list1[2][1][2].append(i)
+
+# print(list1)
+
+
+
+
+# list1 = [5,20,15,20 ,25,50,20]
+
+# list2 = []
+
+# for i in list1 :
+#     if i != 20:
+#         list2.append(i)
+
+# print(list2)
 
     
 
 
-while 20 in list1 :
-    list1.remove(20)
+# while 20 in list1 :
+#     list1.remove(20)
 
-print(list1)
+# print(list1)
+
