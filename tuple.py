2af669076c6690bbf7115763c46fbab0ce9cfdc5
Hasan AbouMinyar <among.00.12@gmail.com>
2025-04-28 06:03:11 +0200

    end of tuple loops

--- tuple.py
+++ tuple.py
@@ -1,6 +1,21 @@
 alf = ("a","b","c")
 
 
+for i in alf: 
+    print(i)
+
+print("\n range len")
+
+for s in range(len(alf)):
+    print(alf[s])
+
+print("\n this a new line")
+
+d = 0 
+while d < len(alf):
+    print(alf[d])
+    d+=1
+
 # this how to change on tuple
 # l= list(alf)
 # l[0]= "google"
