d75a3e3c07d4dd7bde00c637d8a49c84542c821a
Hasan AbouMinyar <among.00.12@gmail.com>
2025-04-28 06:53:24 +0200

    done

---
+++ extar-example.py
@@ -0,0 +1,39 @@
+tuple1 = (10, 20, 30, 40, 50)
+
+reversed_tuple = tuple1[::-1]
+print(reversed_tuple)
+
+
+
+# tuple1 = (10, 20, 30, 40, 50)
+
+# gool = tuple(reversed(tuple1))
+# print(gool)
+
+
+
+# tuple1 = (11, 22)
+# tuple2 = (99, 88)
+
+
+# tuple1 , tuple2 = tuple2 ,tuple1
+
+# print(tuple1)
+# print(tuple2)
+
+
+# numbers = [1, 2, 3, 4, 5, 6, 7]
+
+# squared_numbers = [num**2 for num in numbers]
+# print(squared_numbers)
+
+
+
+
+# numbers = [1, 2, 3, 4, 5, 6, 7]
+
+# squared_numbers = []
+# for num in numbers:
+#     squared_numbers.append(num ** 2)
+
+# print(squared_numbers)
