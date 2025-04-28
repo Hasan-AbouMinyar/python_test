96e0eee3e51c79022be35031bdd57887a99daf27
Hasan AbouMinyar <among.00.12@gmail.com>
2025-04-28 08:06:20 +0200

    files delate

---
+++ files.py
@@ -0,0 +1,16 @@
+f= open("filee.txt","w")
+
+f.write(" software engineering")
+
+
+f.close()
+
+# import os 
+ 
+# if os.path.exists("filee.txt"):
+#     os.remove("filee.txt")
+# else:
+#     print("The file dosen't exist")
+
+# import os 
+# os.remove("filee.txt")
