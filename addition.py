1615d596b16cd156c7f658b64b1c308e7007c729
Hasan AbouMinyar <among.00.12@gmail.com>
2025-04-28 05:18:48 +0200

    done list copies

--- addition.py
+++ addition.py
@@ -5,6 +5,34 @@ w = ["hasan","ahmed" ,"mohamed","majd", "abouminyar"]
 m = [ 2000,"hasan",24,"libyan"]
 
 
+# print(m)
+# m1 = m.copy()
+# print(m1)
+
+# m2= list(m)
+# print(m2)
+
+# m3= m[:]
+# print(m3)
+
+# fuck = m
+# print(fuck)
+
+# fuck.append(22)
+# print("\n this how to work")
+# print(m)
+# print(fuck)
+
+# print (m1)
+# w.reverse()
+# print(w)
+
+# w.sort(reverse=True)
+# print(w)
+
+# w.sort()
+# print(w)
+
 
 # print(m)
 
