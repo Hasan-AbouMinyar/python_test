ee3c653bf0a77777ad76b764b751357523f98293
Hasan AbouMinyar <among.00.12@gmail.com>
2025-05-06 01:27:55 +0200

    v

--- admin.py
+++ admin.py
@@ -5,4 +5,12 @@ print (' just a test to run the startup file ')
 name = 'admin '
 def call ():
     print (name +" his run the call function ")
+
+
+if name == 'ahmed' :
+    print ('you pass the login validation')
+else:
+    print ('the name is '+ name  +' can not pass the valication')
+
+
     
