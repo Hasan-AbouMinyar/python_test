abbd0b7ff034787efc43e85f48309a93b96793d6
Hasan AbouMinyar <among.00.12@gmail.com>
2025-07-04 17:22:24 +0200

    Recon

---
+++ first shot/types.py
@@ -0,0 +1,13 @@
+name  = "hasan "
+
+age = 3 
+
+print (name + " is " + str(age) + " years old")
+
+print ( name , "is ", age , "years old")
+
+
+if age > 18 : 
+    print (name , "is an adult")
+else : 
+    print (name , "is a teenager")
