5517fd2e1d6e5f52f96064ab4e750400601465d1
Hasan AbouMinyar <among.00.12@gmail.com>
2025-07-04 19:43:28 +0200

    vis done

---
+++ first shot/variables.py
@@ -0,0 +1,38 @@
+v = "some text "
+
+print (f"{v}is a some tooo")
+
+number = 11 
+
+print ( f"{number} is a number")
+print ( number, "is a number")
+
+z = 3j
+
+print (z)
+
+str_val = str("Hello")       # str
+int_val = int(20)            # int
+float_val = float(20.5)      # float
+complex_val = complex(1j)    # complex
+list_val = list(("a", "b"))  # list
+tuple_val = tuple(("x", "y"))# tuple
+dict_val = dict(name="John", age=36) # dict
+set_val = set(("a", "b"))    # set
+bool_val = bool(5 > 2)       # bool (True)
+bytes_val = bytes(5)         # bytes
+none_val = None              # NoneType
+
+# دالة type() لمعرفة نوع المتغير.
+print(f"{type(str_val)}")
+print(f"{type(int_val)}")
+print(f"{type(float_val)}")
+print(f"{type(list_val)}")
+print(f"{type(bool_val)}")
+print(f"{type(none_val)}")
+
+
+os = 10 
+print (f"{os}")
+nos = float(os)
+print (f"{nos}")
