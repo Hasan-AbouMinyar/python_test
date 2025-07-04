bd4da1cb0609868c1e9d33d8da7976d201faf8f3
Hasan AbouMinyar <among.00.12@gmail.com>
2025-07-04 17:16:55 +0200

    first done for today

--- first shot/trial.py
+++ first shot/trial.py
@@ -1,1 +1,21 @@
-print("Hello, World!")
+if 5 > 2:
+    print("Five is greater than two!")
+    print("Five is greater than two!")
+
+if 5 > 2: 
+    print ( " the five is greater then two !")
+
+
+
+a = 3 
+b = 9
+c = 7
+e = "Hasan "
+d = "Abouminyar"
+
+print (a,b,c)
+
+print ( a+ b+ c)
+
+print ( e + " " + d)
+
