d505c213a8be63c69b4f0fcd85787dcd3c639311
Hasan AbouMinyar <among.00.12@gmail.com>
2025-07-05 06:47:55 +0200

    oh this is another example

--- files /firstFileShoot.py
+++ files /firstFileShoot.py
@@ -14,3 +14,12 @@
 
 # file.write("Hello, hasan this is a test file!")
 # file.write("\n This is another hasan a new line added")
+
+
+# file  = open("example.txt", "r")
+# print (file.read())
+# file.close()
+
+file = open("examplee.txt", "x")
+file.write("Hello, this is a test file!")
+file.close()
