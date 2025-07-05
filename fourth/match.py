747f6747bb0ec1a0a9500ff9b7b5e3b307e3ca1f
Hasan AbouMinyar <among.00.12@gmail.com>
2025-07-05 06:31:38 +0200

    match file

--- fourth/match.py
+++ fourth/match.py
@@ -1,15 +1,15 @@
 day = 2 
 
-match day:
-    case 1:
-        print ("ONE")
-    case 2:
-        print ("TWO")
-    case 3:
-        print ("THREE")
-    case 4:
-        print ("FOUR")
-    case 5:
-        print ("FIVE")
-    case 6:
-        print ("SIX")
+# match day:
+#     case 1 | 4:
+#         print ("ONE")
+#     case 2:
+#         print ("TWO")
+#     case 3:
+#         print ("THREE")
+#     case 4:
+#         print ("FOUR")
+#     case 5:
+#         print ("FIVE")
+#     case 6:
+#         print ("SIX")
