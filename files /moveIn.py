cc7d90c3d88088a020f04db388b68353c52aedf8
Hasan AbouMinyar <among.00.12@gmail.com>
2025-07-05 06:53:49 +0200

    threeth line

--- files /moveIn.py
+++ files /moveIn.py
@@ -1,3 +1,6 @@
-file = open("example.txt", "r")
-print(file.readline())
-file.close()
+# file = open("example.txt", "r")
+# print(file.readline())
+# file.close()
+
+file = open("example.txt", "a")
+file.write("\n fucking threeth line bro")
