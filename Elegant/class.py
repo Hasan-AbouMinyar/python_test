91ff5227efd958392dfdd620aa51c34d2d5a1b37
Hasan AbouMinyar <among.00.12@gmail.com>
2025-07-05 07:10:19 +0200

    done

---
+++ Elegant/class.py
@@ -0,0 +1,13 @@
+class Me:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        return f"Hello, my name is {self.name} and I am {self.age} years old."
+        
+
+i = Me("Hasan", 25)
+
+i.greet()
+print(i.greet())
