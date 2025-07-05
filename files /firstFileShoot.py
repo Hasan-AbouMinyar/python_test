80952b3cd8419db63ee1cc80d7450cbd5fd19f41
Hasan AbouMinyar <among.00.12@gmail.com>
2025-07-05 06:45:54 +0200

    what

--- files /firstFileShoot.py
+++ files /firstFileShoot.py
@@ -10,7 +10,7 @@
 # file.write("\n This is a new line added")
 # file.close()
 
-file = open("example.txt", "w")
+# file = open("example.txt", "w")
 
-file.write("Hello, hasan this is a test file!")
-file.write("\n This is another hasan a new line added")
+# file.write("Hello, hasan this is a test file!")
+# file.write("\n This is another hasan a new line added")
