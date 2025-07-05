ce6b60afbd3847aa0f06867043ab625220b2fd4f
Hasan AbouMinyar <among.00.12@gmail.com>
2025-07-05 06:45:34 +0200

    deleted

--- files /firstFileShoot.py
+++ files /firstFileShoot.py
@@ -6,6 +6,11 @@
 # print(file.read())
 # file.close()
 
-file = open("example.txt", "a")
-file.write("\n This is a new line added")
-file.close()
+# file = open("example.txt", "a")
+# file.write("\n This is a new line added")
+# file.close()
+
+file = open("example.txt", "w")
+
+file.write("Hello, hasan this is a test file!")
+file.write("\n This is another hasan a new line added")
