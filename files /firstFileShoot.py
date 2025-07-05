d9dc3fbc850c48faf56bd39b1b9b8c0b5cc4bdd8
Hasan AbouMinyar <among.00.12@gmail.com>
2025-07-05 06:42:01 +0200

    wow i have just creat a file wiht text

--- files /firstFileShoot.py
+++ files /firstFileShoot.py
@@ -1,4 +1,11 @@
+# file = open("example.txt", "w")
+# file.write("Hello, this is a test file!")
+# file.close()
 
-file = open("example.txt", "r")
-print(file.read())   # قراءة محتوى الملف بالكامل
+# file = open("example.txt", "r")
+# print(file.read())
+# file.close()
+
+file = open("example.txt", "a")
+file.write("\n This is a new line added")
 file.close()
