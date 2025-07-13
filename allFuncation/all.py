36913ea01debb1b99c9b6206589ea64e3e7c7736
Hasan AbouMinyar <among.00.12@gmail.com>
2025-07-14 01:32:43 +0200

    all fucntion with any

---
+++ allFuncation/all.py
@@ -0,0 +1,15 @@
+array = [1,2,3,4,33,0,31,42]
+
+st = ['abc','cde','']
+
+bool = [True, True, True, 3]
+
+ALL = [False, False, False, False]
+
+print (all(array))
+print (all(st))
+print (all(bool))
+
+print (any(array))
+
+print (any(ALL))
