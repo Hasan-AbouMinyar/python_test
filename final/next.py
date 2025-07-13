3a5e73ab2d9c3f26e75c31702b12ad3df5d4f2ef
Hasan AbouMinyar <among.00.12@gmail.com>
2025-07-13 19:30:17 +0200

    next

--- final/next.py
+++ final/next.py
@@ -1,12 +1,14 @@
 import mysql.connector
 
-ssqqll = mysql.connector.connect(
+conn = mysql.connector.connect(
     host="localhost",
     user="root",
     password="",
+    database="py_test",
 )
 
+ex = conn.cursor()
 
-db_name = ssqqll.cursor()
-
-db_name.execute("create database if not exists py_test")
+ex.execute("insert into users (name, age, address) values ('John', 30,'Tripoli'), ('Jane', 25,'Benghazi') ,('Doe', 22,'Misrata'), ('Alice', 28,'Sirt'), ('Bob', 35,'Derna')")
+conn.commit()
+print("Data inserted successfully")
