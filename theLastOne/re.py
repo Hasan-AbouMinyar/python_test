906aecc0ce61500df634b964a4db1c0152aa8f77
Hasan AbouMinyar <among.00.12@gmail.com>
2025-07-14 03:29:51 +0200

    re

--- theLastOne/re.py
+++ theLastOne/re.py
@@ -1,2 +1,9 @@
 import re
 
+text = "Hasan Abouminyar is a Dev of Software engeaneer"
+
+# result = re.findall("hasan",text, re.IGNORECASE)
+# print(result)
+
+s = re.search("a",text, re.IGNORECASE)
+print(s)    
