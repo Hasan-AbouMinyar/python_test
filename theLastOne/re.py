7ae7abffa3e2cbb81892a2cf424cfcca3d3471a8
Hasan AbouMinyar <among.00.12@gmail.com>
2025-07-14 05:50:59 +0200

    yes it is

--- theLastOne/re.py
+++ theLastOne/re.py
@@ -5,5 +5,8 @@ text = "Hasan Abouminyar is a Dev of Software engeaneer"
 # result = re.findall("hasan",text, re.IGNORECASE)
 # print(result)
 
-s = re.search("a",text, re.IGNORECASE)
-print(s)    
+s = re.search("D",text)
+print(s.start())    
+
+me = re.sub("a","A",text)
+print(me)
