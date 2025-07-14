f1c2e48402ea91f845dc073769d285e2a84d4aac
Hasan AbouMinyar <among.00.12@gmail.com>
2025-07-14 07:29:46 +0200

    yse add

--- folder/file.py
+++ folder/file.py
@@ -1,5 +1,8 @@
 file = open("json.txt", "a")
-file.write("This is a new line.\n")
+file.write("Hi there what in the world this is a test.\n")
+file.close()
+
+file = open("json.txt", "r")
 me = file.read()
 print(me)
 file.close()
