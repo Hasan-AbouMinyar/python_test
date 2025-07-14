c118f069d64c546b1b701155387448024879b1f7
Hasan AbouMinyar <among.00.12@gmail.com>
2025-07-14 02:12:54 +0200

    commit done

---
+++ allFuncation/enumrate.py
@@ -0,0 +1,14 @@
+array = ["Hasan", "Ahmed", "Abouminyar"]
+
+print(list(enumerate(array)))
+
+print("  ")
+
+
+for index , value in enumerate(array, start = 1):
+    print(f"FR{index}  {value}")
+
+
+
+
+
