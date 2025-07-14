0bf1643833bbeb25fba44f33824310dc511c64c1
Hasan AbouMinyar <among.00.12@gmail.com>
2025-07-14 06:16:49 +0200

    task

---
+++ list/go.py
@@ -0,0 +1,14 @@
+todoList = ["study python", "go to the gym", "buy groceries"]
+
+todoList.append ("read a book")
+
+print (todoList)
+
+
+todoList.remove ("go to the gym")
+
+print("yourt task to day is : ")
+
+for task in todoList:
+    print ("_ " + task)
+
