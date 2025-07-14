4b09c6c2398be9c6a6472d7a7b58931e66185bec
Hasan AbouMinyar <among.00.12@gmail.com>
2025-07-14 06:20:00 +0200

    last order

---
+++ list/order.py
@@ -0,0 +1,13 @@
+orders = []
+
+
+orders.append("Iphone 14")
+orders.append("Macbook Pro")
+orders.append("Ipad Pro")
+print (orders)
+
+last_order = orders.pop()
+
+print(f"Last order: {last_order}")
+print ("orders: ", orders  )
+print("last processed order: ", last_order)
